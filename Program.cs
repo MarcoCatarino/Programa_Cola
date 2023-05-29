c3aa270237648b4d6b8033bb7cc45dd4060977ad
MarcoCatarino <133799605+MarcoCatarino@users.noreply.github.com>
2023-05-29 11:19:19 -0500

    Update Program.cs
    Las colas son aquellas de las cuales no sabemos su fin.

--- Program.cs
+++ Program.cs
@@ -44,6 +44,67 @@ namespace ListasTipoCola
                 raiz = nuevo;
                 fondo = nuevo;
             }
+            else
+            {
+                fondo.sig = nuevo;
+                fondo = nuevo;
+            }
+        }
+
+        public int Extraer()
+        {
+            if (!Vacia())
+            {
+                int informacion = raiz.info;
+
+                if (raiz == fondo)
+                {
+                    raiz = null;
+                    fondo = null;
+                }
+                else
+                {
+                    raiz = raiz.sig;
+                }           
+
+                return informacion;
+            
+            }
+            return int.MaxValue;    // En caso de que no sea una de las opciones regresara el numero maximo
+        }
+
+        public void Imprimir()
+        {
+            Nodo reco = raiz;
+            Console.WriteLine("Listado de los elementos > ");
+
+            while (reco != null)
+            {
+                Console.WriteLine(reco.info + "- ");
+                reco = reco.sig;
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Cola estructura = new();
+
+            estructura.Insertar(2);
+            estructura.Insertar(3);
+            estructura.Insertar(6);
+            estructura.Insertar(7);
+            estructura.Insertar(9);
+            estructura.Insertar(10);
+
+            estructura.Imprimir();
+
+            Console.WriteLine("Extraemos un elemento de la estructura > " + estructura.Extraer());
+
+            estructura.Imprimir();
+
+            Console.ReadKey();
+
         }
     }
 }
